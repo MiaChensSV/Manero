a5064b691d3191113d7d8ef640aca2cfdec70e06
DanielAndersson31 <111743042+DanielAndersson31@users.noreply.github.com>
2023-10-13 11:26:19 +0200

    Cleanup

--- Program.cs
+++ Program.cs
@@ -14,8 +14,6 @@ namespace Manero
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
