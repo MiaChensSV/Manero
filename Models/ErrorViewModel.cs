03e36d8c9a727b45060d5e9ec08e3e2eb08ff9aa
Mia Chen <111744515+MiaChensSV@users.noreply.github.com>
2023-10-17 10:08:18 +0200

    Delete Models directory
    clean up

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Manero.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
