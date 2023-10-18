51ec07f7e54ab5b907d0376937b85c0cfd21118a
MiaChensSV <miachensv@gmail.com>
2023-10-18 14:55:11 +0200

    add UI for myaddress

---
+++ Controllers/AddressController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Manero.Controllers
+{
+    public class AddressController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
