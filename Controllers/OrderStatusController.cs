168c89feadb3176f6380a485f7173747499850b0
nikoradic <nikolaradic94@hotmail.com>
2023-10-19 12:25:24 +0200

    Created Success/Failed Message

---
+++ Controllers/OrderStatusController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Manero.Controllers
+{
+    public class OrderStatusController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
