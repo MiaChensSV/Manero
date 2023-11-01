d7ca45b15e9607b226c5ee15873a2803f150c35e
Thomas Palmqvist <thomas_palmqvist@live.se>
2023-11-01 11:59:33 +0100

    Created a methode for adding promocode to user

--- Manero/Controllers/PromoCodeController.cs
+++ Manero/Controllers/PromoCodeController.cs
@@ -1,41 +1,62 @@
 using Manero.Repository.PromoCodeRepo;
 using Microsoft.AspNetCore.Mvc;
+using MySqlX.XDevAPI.Common;
+using Org.BouncyCastle.Crypto;
 using System.Security.Claims;
 
 namespace Manero.Controllers
 {
-    public class PromoCodeController : Controller
-    {
-        private readonly GetUserPromoCodeRepo _repo;
+	public class PromoCodeController : Controller
+	{
+		private readonly GetUserPromoCodeRepo _repo;
 
-        public PromoCodeController(GetUserPromoCodeRepo repo)
-        {
-            _repo = repo;
-        }
+		public PromoCodeController(GetUserPromoCodeRepo repo)
+		{
+			_repo = repo;
+		}
 
-        public async Task<IActionResult> PromoCodesCurrent()
-        {
-            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            if (userId == null)
-            {
-                return RedirectToAction("Index", "Login");
-            }
-            var promocode = await _repo.GetUserPromocodesAsync(userId);
-            if (!promocode.Any())
-            {
-                return RedirectToAction("PromoCodeEmpty", "PromoCode");
-            }
-            return View(promocode);
-        }
+		public async Task<IActionResult> PromoCodesCurrent()
+		{
+			var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+			if (userId == null)
+			{
+				return RedirectToAction("Index", "Login");
+			}
+			var promocode = await _repo.GetUserPromocodesAsync(userId);
+			if (!promocode.Any())
+			{
+				return RedirectToAction("PromoCodeEmpty", "PromoCode");
+			}
+			return View(promocode);
+		}
 
-        public IActionResult PromoCodesUsed()
-        {
-            return View();
-        }
 
-        public IActionResult PromoCodeEmpty()
-        {
-            return View();
-        }
-    }
+		public IActionResult PromoCodesUsed()
+		{
+			return View();
+		}
+
+		public IActionResult PromoCodeEmpty()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> AddPromoCodeToUser(string promoCodeTitle)
+		{
+			var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+			bool result = await _repo.AssignPromoCodeToUserAsync(userId, promoCodeTitle);
+
+			if (!result)
+			{
+				ViewData["ErrorMessage"] = "Invalid or duplicate promo code.";
+				return View("PromoCodeEmpty", "PromoCode");
+			}
+			else
+			{
+				return RedirectToAction("PromoCodeCurrent", "PromoCode");
+			}
+
+		}
+	}
 }
--- Manero/Repository/PromoCodeRepository/GetUserPromoCodeRepo.cs
+++ Manero/Repository/PromoCodeRepository/GetUserPromoCodeRepo.cs
@@ -29,5 +29,54 @@ public class GetUserPromoCodeRepo : GeneralRepo<PromocodesEntity>
 
         return new List<PromocodesEntity>();
     }
+
+
+	public async Task<bool> AssignPromoCodeToUserAsync(string userId, string promoCodeTitle)
+	{
+		using var transaction = await _context.Database.BeginTransactionAsync();
+
+		try
+		{
+			// 1. Kontrollera om promocoden existerar.
+			var promoCodeEntity = await _context.Promocodes
+				.Where(x => x.PromocodeTitle == promoCodeTitle)
+				.FirstOrDefaultAsync();
+
+			if (promoCodeEntity == null)
+			{
+				return false; // Promocode finns inte.
+			}
+
+			// 2. Kontrollera om användaren redan har den promocoden.
+			var existingUserPromo = await _context.UserPromocodes
+				.Where(x => x.UserId == userId && x.PromocodeId == promoCodeEntity.PromocodeId)
+				.FirstOrDefaultAsync();
+
+			if (existingUserPromo != null)
+			{
+				return false; // Användaren har redan denna promocode.
+			}
+
+			// 3. Lägg till en ny post för att koppla användaren med promocoden.
+			var newUserPromo = new UserPromocodesEntity
+			{
+				UserId = userId,
+				PromocodeId = promoCodeEntity.PromocodeId
+			};
+
+			_context.UserPromocodes.Add(newUserPromo);
+			await _context.SaveChangesAsync();
+
+			await transaction.CommitAsync();
+
+			return true; // Allt gick bra.
+		}
+		catch (Exception ex)
+		{
+			await transaction.RollbackAsync();
+			throw; // eller logga felet och returnera false.
+		}
+	}
+
 }
 
