555400bcb5abbcf481c990b913c0f8fd17ed74ec
MiaChensSV <miachensv@gmail.com>
2023-11-06 10:30:29 +0100

    update-database

--- Manero/Migrations/20231106090212_FK-OrderDetailEntity.Designer.cs
+++ Manero/Migrations/20231106090212_FK-OrderDetailEntity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Manero.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Manero.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20231106090212_FK-OrderDetailEntity")]
+    partial class FKOrderDetailEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
