ea4bb4606fbb3715c88f0abfeb9d1231044e87a9
MiaChensSV <miachensv@gmail.com>
2023-11-06 10:01:37 +0100

    Add shoppingCartController/service/repository/ Change foreign key in orderDetailEntity

---
+++ Manero/Repository/ShoppingCartRepository.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq.Expressions;
+using Manero.Context;
+using Manero.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using MySqlX.XDevAPI.Relational;
+
+namespace Manero.Repository
+{
+	public class ShoppingCartRepository : GeneralRepo<OrderDetailEntity>
+	{
+		private readonly DataContext _context;
+
+		public ShoppingCartRepository(DataContext context) : base(context)
+		{
+			_context = context;
+		}
+
+		public async Task<IEnumerable<OrderDetailEntity>> GetCartByUser(string userId)
+		{
+			int _orderId = GetOrderIdByUser(userId).Result;
+			var _orderDetailList = await _context.OrderDetail
+				.Where(row => row.OrderId == _orderId)
+				.ToListAsync();
+			return _orderDetailList;
+		}
+
+		public async Task UpdateCartByUser(OrderDetailEntity cartItem)
+		{
+			_context.OrderDetail.Update(cartItem);
+			await _context.SaveChangesAsync();
+		}
+		public async Task DeleteCartItemByUser(OrderDetailEntity cartItem)
+		{
+			_context.OrderDetail.Remove(cartItem);
+			await _context.SaveChangesAsync();
+		}
+
+		private async Task<int> GetOrderIdByUser(string userId)
+		{
+			var _orderDetails = await _context.OrderDetails
+				.Where(row => row.UserId == userId)
+				.Where(row => row.StatusId == 1)
+				.OrderBy(row => row.Modified_Date)
+				.LastOrDefaultAsync();
+			if (_orderDetails != null)
+				return _orderDetails.OrderId;
+			else
+				return -1;
+		}
+	}
+}
---
+++ Manero/Services/ShoppingCartService.cs
@@ -0,0 +1,29 @@
+﻿using Manero.Models.Entities;
+using Manero.Repository;
+
+namespace Manero.Services;
+
+public class ShoppingCartService
+{
+	private readonly ShoppingCartRepository _cartRepo;
+
+	public ShoppingCartService(ShoppingCartRepository cartRepo)
+	{
+		_cartRepo = cartRepo;
+	}
+
+	public async Task<IEnumerable<OrderDetailEntity>> GetCartByUserAsync(string userId)
+	{
+		return await _cartRepo.GetCartByUser(userId);
+	}
+
+	public async Task UpdateCartByUserAsnyc(OrderDetailEntity newItem)
+	{
+		await _cartRepo.UpdateCartByUser(newItem);
+	}
+
+	public async Task DeleteCartItemByUserAsync(OrderDetailEntity item)
+	{
+		await _cartRepo.DeleteCartItemByUser(item);
+	}
+}
