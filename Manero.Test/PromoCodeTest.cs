c5334ceb4dd7ce0fe9d6d2dfff51e8e4de77b5e7
PrHiGo <thomas_palmqvist@live.se>
2023-11-07 20:14:36 +0100

    Trying to create a test

---
+++ Manero.Test/PromoCodeTest.cs
@@ -0,0 +1,40 @@
+﻿using MailChimp.Net.Models;
+using Manero.Controllers;
+using Manero.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manero.Test;
+
+public class PromoCodeTest
+{
+    [Fact]
+    public async Task PromoCodesCurrent_ReturnsRedirectToLogin_WhenUserIdIsNull()
+    {
+        //Arrange
+        var mockRepo = new Mock<PromoCodeRepo>();
+        var controllerContext = new ControllerContext
+        {
+            HttpContext = new DefaultHttpContext { User = new ClaimsPrincipal() }
+        };
+        var controller = new PromoCodeController(mockRepo.Object)
+        {
+            ControllerContext = controllerContext
+        };
+
+        //Act
+        var result = await controller.PromoCodesCurrent();
+
+        //Assert 
+        var redirectResult = Assert.IsType<RedirectToActionResult>(result);
+        Assert.Equal("Index", redirectResult.ActionName);
+        Assert.Equal("Login", redirectResult.ControllerName);
+    }
+}
--- Manero/Controllers/PromoCodeController.cs
+++ Manero/Controllers/PromoCodeController.cs
@@ -56,7 +56,6 @@ namespace Manero.Controllers
 			{
 				return RedirectToAction("PromoCodesCurrent", "PromoCode");
 			}
-
 		}
 	}
 }
