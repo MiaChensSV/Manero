1834f6b4ce7cf66b8a23e0f0ab1de057f0c962f8
Emmvid <emmavidarsson@hotmail.com>
2023-11-14 10:10:37 +0100

    added working test on productproductdetail, need to work on productscontroller

--- Manero.Test/ProductControllerTests.cs
+++ Manero.Test/ProductControllerTests.cs
@@ -1,24 +1,31 @@
-﻿using Manero.Controllers;
+﻿using Manero.Context;
+using Manero.Controllers;
 using Manero.Models.Dtos;
+using Manero.Models.Entities;
 using Manero.Repository;
 using Manero.Services;
 using Manero.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Moq;
+using System.Linq.Expressions;
 
 namespace Manero.Test
 {
     
     public class ProductController_Tests
     {
-       
+      
         private readonly Mock<IProductListService> _productListService;
         private readonly ProductsController _productsController;
         private readonly ProductDetailsRepo _productDetailsRepo;
 
         public ProductController_Tests()
         {
+          
+            
             _productListService = new Mock<IProductListService>();
+            
             _productsController = new ProductsController(_productListService.Object, _productDetailsRepo);
 
            
@@ -54,7 +61,35 @@ namespace Manero.Test
             Assert.NotNull(viewModel.AllProducts);
             
 
-        }       
+        }
+        // EMMAS TEST
+        [Fact]
+        public async Task ProductsController_Details_ShouldReturnViewWithProduct()
+        {
+            // Arrange
+            int productId = 1;
+            var product = new ProductProductDetail
+            {
+                ArticleNumber = "123",
+                ProductTitle = "En Produkt",
+                Price = 999,
+                Image = "bild.jpg",
+                
+            };
+
+
+            // Act
+            var result = await _productsController.Details(productId) as ViewResult;
+
+            // Assert
+            Assert.NotNull(result);
+
+            var viewModel = result.Model as ProductProductDetail;
+            Assert.NotNull(viewModel);
+            Assert.Equal(product.ArticleNumber, viewModel.ArticleNumber);
+            Assert.Equal(product.ProductTitle, viewModel.ProductTitle);
+            
+        }
     }
     
 }
